92a5bdc0ac03905514be362a7f6eed0c88eaa11b
Gustavo <gustavo.tatarem@saedigital.com.br>
2020-07-20 23:41:46 -0300

    Add verification for same date booking

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,17 +1,36 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentRouter = Router();
 
-const appointments = [];
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: Appointment[] = [];
 
 appointmentRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findSameDateAppointment = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  if (findSameDateAppointment) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked.' });
+  }
+
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   };
 
   appointments.push(appointment);
