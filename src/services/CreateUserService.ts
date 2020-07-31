36ce7b07e9cec97869eee3804f66c39b0b6a66e5
Gustavo Tatarem <gus.tatarem@gmail.com>
2020-07-30 21:24:35 -0300

    password encrypt

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,4 +1,5 @@
 import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
 
 import User from '../models/User';
 
@@ -12,10 +13,20 @@ class CreateUserService {
   public async execute({ name, email, password }: Request): Promise<User> {
     const usersRepository = getRepository(User);
 
+    const checkIfUserExists = await usersRepository.findOne({
+      where: { email },
+    });
+
+    if (checkIfUserExists) {
+      throw new Error('This email is already being used.');
+    }
+
+    const hashedPassword = await hash(password, 8);
+
     const user = usersRepository.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
 
     await usersRepository.save(user);
