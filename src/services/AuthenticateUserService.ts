4e4a2106810ff53ad4d29c3953c162dc26b9dec6
Gustavo Tatarem <gus.tatarem@gmail.com>
2020-08-04 17:50:58 -0300

    create user auhtentication service

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,42 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
+import User from '../models/User';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+  token: string;
+}
+
+class AuthenticateUserService {
+  public async execute({ email, password }: Request): Promise<Response> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error('Incorrect email/password combination');
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error('Incorrect email/password combination');
+    }
+
+    const token = sign({}, '6b63f22ba22cde70af2c676c9e4bee12', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return { user, token };
+  }
+}
+
+export default AuthenticateUserService;
