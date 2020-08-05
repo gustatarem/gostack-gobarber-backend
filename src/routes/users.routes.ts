f171135e0d7be0cc696a2fa3960cff7e32da9314
Gustavo Tatarem <gus.tatarem@gmail.com>
2020-08-05 20:10:58 -0300

    Update user avatar config

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import multer from 'multer';
 import uploadConfig from '../config/upload';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
@@ -29,9 +31,23 @@ usersRouter.post('/', async (request, response) => {
 
 usersRouter.patch(
   '/avatar',
+  ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    return response.json({ ok: true });
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      delete user.password;
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   },
 );
 
