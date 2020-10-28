059e2edb8cdfa96030d357ea332d1fa83cd0c46b
Gustavo Tatarem <gus.tatarem@gmail.com>
2020-10-28 17:33:52 -0300

    Make earlier hours in a day unavailable

--- src/modules/appointments/services/ListProviderDayAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.ts
@@ -1,9 +1,8 @@
 import { inject, injectable } from 'tsyringe';
-// import { getDaysInMonth, getDate } from 'date-fns';
+import { getHours, isAfter } from 'date-fns';
 
 // import AppError from '@shared/errors/AppError';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
-import { getHours } from 'date-fns';
 
 interface IRequest {
   provider_id: string;
@@ -46,14 +45,18 @@ class ListProviderDayAvailabilityService {
       (_, index) => index + hourStart,
     );
 
+    const currentDate = new Date(Date.now());
+
     const availability = eachHourArray.map(hour => {
       const hasAppointmentInHour = appointments.find(
         appointment => getHours(appointment.date) === hour,
       );
 
+      const compareDate = new Date(year, month - 1, day, hour);
+
       return {
         hour,
-        available: !hasAppointmentInHour,
+        available: !hasAppointmentInHour && isAfter(compareDate, currentDate),
       };
     });
 
