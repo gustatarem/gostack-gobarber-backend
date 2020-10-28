98e299b5ed7fa74cd500dd17622c8050eabda4f4
Gustavo Tatarem <gus.tatarem@gmail.com>
2020-10-28 17:20:23 -0300

    List available hours in a day

---
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -0,0 +1,44 @@
+// import AppError from '@shared/errors/AppError';
+
+import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
+import ListProviderDayAvailabilityService from './ListProviderDayAvailabilityService';
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let listProviderDayAvailability: ListProviderDayAvailabilityService;
+
+describe('ListProviderDayAvailability', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProviderDayAvailability = new ListProviderDayAvailabilityService(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  it('should be able to list the day avaliability from provider', async () => {
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 10, 20, 8, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 10, 20, 10, 0, 0),
+    });
+
+    const avaliability = await listProviderDayAvailability.execute({
+      provider_id: 'user',
+      day: 20,
+      month: 11,
+      year: 2020,
+    });
+
+    expect(avaliability).toEqual(
+      expect.arrayContaining([
+        { hour: 8, available: false },
+        { hour: 9, available: true },
+        { hour: 10, available: false },
+        { hour: 11, available: true },
+      ]),
+    );
+  });
+});
---
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.ts
@@ -0,0 +1,64 @@
+import { inject, injectable } from 'tsyringe';
+// import { getDaysInMonth, getDate } from 'date-fns';
+
+// import AppError from '@shared/errors/AppError';
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import { getHours } from 'date-fns';
+
+interface IRequest {
+  provider_id: string;
+  day: number;
+  month: number;
+  year: number;
+}
+
+type IResponse = Array<{
+  hour: number;
+  available: boolean;
+}>;
+
+@injectable()
+class ListProviderDayAvailabilityService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
+
+  public async execute({
+    provider_id,
+    day,
+    year,
+    month,
+  }: IRequest): Promise<IResponse> {
+    const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
+      {
+        provider_id,
+        day,
+        year,
+        month,
+      },
+    );
+
+    const hourStart = 8;
+
+    const eachHourArray = Array.from(
+      { length: 10 },
+      (_, index) => index + hourStart,
+    );
+
+    const availability = eachHourArray.map(hour => {
+      const hasAppointmentInHour = appointments.find(
+        appointment => getHours(appointment.date) === hour,
+      );
+
+      return {
+        hour,
+        available: !hasAppointmentInHour,
+      };
+    });
+
+    return availability;
+  }
+}
+
+export default ListProviderDayAvailabilityService;
