5d43f53a8f31a3f40423c1b87716ea60c34981be
Gustavo Tatarem <gus.tatarem@gmail.com>
2020-10-29 17:14:10 -0300

    configure s3 for file uploading (production)

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -11,7 +11,7 @@ import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 const userAvatarConroller = new UserAvatarController();
 const usersController = new UsersController();
 const usersRouter = Router();
-const upload = multer(uploadConfig);
+const upload = multer(uploadConfig.multer);
 
 usersRouter.post(
   '/',
